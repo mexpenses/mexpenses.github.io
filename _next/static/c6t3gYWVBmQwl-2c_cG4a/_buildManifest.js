2b796a459fa466a6ca42f8daba56855dfdb4a791
mexpenses <115945287+mexpenses@users.noreply.github.com>
2022-10-18 14:19:18 +0000

    Deploying to gh-pages from  @ f95115bb7c5e0ecb366f5c4398c693966ba68f8c 🚀

--- _next/static/c6t3gYWVBmQwl-2c_cG4a/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/677-5f3004b426aa4415.js","static/chunks/pages/index-1470f776082b5aad.js"],"/_error":["static/chunks/pages/_error-3c9a37b9888d2d4e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
