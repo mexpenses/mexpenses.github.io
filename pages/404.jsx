fb67e0432372d4d276aafbed3d2788506911fe73
Hector Silva <hectddos103@gmail.com>
2024-06-08 14:54:44 -0300

    fix: alterando text-color da página not-found

--- pages/404.jsx
+++ pages/404.jsx
@@ -7,7 +7,7 @@ export default function NotFound() {
         <main className="flex min-h-screen justify-center items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
             <div className="text-center">
                 <Image src={LogoMexpenses} className="mx-auto" width={150} height={150} alt="Mexpenses"/>
-                <h1 className="mt-4 text-3xl font-bold tracking-tight text-indigo-500 sm:text-5xl">Página não encontrada</h1>
+                <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-800 sm:text-5xl">Página não encontrada</h1>
                 <p className="mt-6 text-base leading-7 text-gray-600">Lamentamos, mas não conseguimos encontrar a página que procura.</p>
                 <div className="mt-10 flex items-center justify-center gap-x-6">
                     <Link href="/" className="text-sm font-semibold text-gray-900 hover:text-gray-600">
